ec5b60a9db7b7aac6a5e2f17f5ee0811b3377453
Dean Howe <hideanhowe@gmail.com>
2025-12-28 21:14:59 +0000

    testing

--- index.js
+++ index.js
@@ -6,3 +6,16 @@
 // Return an array of strings with each attribute property and value, separated by a comma, in this format: ["attribute1, value1", "attribute2, value2"].
 // Return attributes in the order they are given.
 // If no attributes are found, return an empty array.
+
+function extractAttributes(element) {
+  console.log(element);
+  // return an array of strings
+  // ["attribute1, value1", "attribute2, value2"]
+  // space text equals quotes text quotes
+  const regex = /^[\s]([a-zA-Z]*)[\=][\"\']([a-zA-Z]*)[\"\']/g;
+
+  const test = regex.test(element);
+  console.log(test);
+
+  // return element;
+}
